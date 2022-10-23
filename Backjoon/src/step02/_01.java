3248fbfb641174fbf0329c48ca59d52f42712d1a
jun <jungy0309@@naver.com>
2022-10-23 22:17:06 +0900

    2022/10/23

--- Backjoon/src/step02/_01.java
+++ Backjoon/src/step02/_01.java
@@ -1,5 +1,23 @@
 package step02;
 
+import java.util.Scanner;
+/*
+ * 날짜 : 2022/10/23
+ * 이름 : 라성준
+ * 내용 : 백준문제 2단계 1번 문제 두 수 비교하기
+ */
 public class _01 {
-
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+					
+		if(A>B) System.out.println(">");
+		
+		else if (A<B) System.out.println("<");
+		
+		else System.out.println("==");
+	}
 }
